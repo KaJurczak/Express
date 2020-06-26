7b02c14189ae2e6cf26152ae7c5059a5059431b0
Karolina Jurczak <inka_c@tlen.pl>
2020-06-26 17:23:11 +0200

    Change files

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use('/user', (req, res) => {
+  res.show('forbidden.html'); //there I can write res.send('You need to log in') if HTML file is not necessary
+});
+
 app.get('/', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/home', (req, res) => {
   res.show('index.html');
 });
 
@@ -20,20 +28,8 @@ app.get('/about', (req, res) => {
   res.show('about.html');
 });
 
-app.get('/contact', (req, res) => {
-  res.show('contact.html');
-});
-
-app.get('/info', (req, res) => {
-  res.show('info.html');
-});
-
-app.get('/history', (req, res) => {
-  res.show('history.html');
-});
-
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(404).show('error.html');
 })
 
 app.listen(8000, () => {
